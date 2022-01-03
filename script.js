233213919ff62b92f3378bd08b3c9fe6ee67b4e0
MChambersIV <chambersm025@gmail.com>
2022-01-03 14:30:29 -0800

    added ids to elements in index added 1 line of style in style.css

---
+++ script.js
@@ -0,0 +1,16 @@
+//We want a variable assigned to the wins and another to the losses counter
+//We a variable assigned to the start button
+//We a variable assigned to the dotted line where the objective word will live
+
+//When we load the page we want to check local storage for previous wins and losses
+    //if there are none the counters will display as 0/null/none
+
+//When we push the start button we want to initilize the game
+    //it needs to track the keys the user presses
+
+//We need an array of words to select from randomly like math.floor (math.random)
+//
+
+    //That means want to start the counter
+        //we need and if else depending on if the word was completed succesfully or not in time
+            //we need to update local storage whenever the total score changes
